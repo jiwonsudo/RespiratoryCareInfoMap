55ac2ef3b6392f6a1d8b237fecf9d5781bae0ded
jiwonsudo <wondev112@gmail.com>
2022-03-06 21:44:02 +0900

    Beta로 버전수정, 레이아웃 대략 구성, 화면 Portrait로 고정

--- RespiratoryCareInfoMap/AppDelegate.swift
+++ RespiratoryCareInfoMap/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            
+            // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
+        }
 
     // MARK: UISceneSession Lifecycle
 
